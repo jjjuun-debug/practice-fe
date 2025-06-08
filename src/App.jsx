6f97b9265b410282f658a7db7498d211b0227016
juyounginpark <0620ffff@naver.com>
2025-06-08 11:46:35 +0900

    day1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from 'react';
+import './App.css';
 function App() {
-  const [count, setCount] = useState(0)
 
+  let name = '박주영';
+  let job = '개발자꿈나무';
+  let call = '010-6868-3956';
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="black-nav">
+        <h4 style={{color:'red',fondSize:'16px'}}>
+          명함</h4>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="mainsytle-nav">
+        <h3>{name}</h3>
+        <h4>{job}</h4>
+        <h5>📞{call}</h5>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      
+    </div>
+    
+  );
 }
 
-export default App
+export default App;
