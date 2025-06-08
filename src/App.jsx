4a89e8e25db770b1d9925ef1673335762694ac96
juyounginpark <0620ffff@naver.com>
2025-06-08 11:59:36 +0900

    day2 heart

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import {useState} from 'react';
 function App() {
 
   let name = '박주영';
   let job = '개발자꿈나무';
   let call = '010-6868-3956';
+  let [heart,plus] = useState(0);
+
+  
   return (
     <div className="App">
       <div className="black-nav">
@@ -12,7 +16,7 @@ function App() {
           명함</h4>
       </div>
       <div className="mainsytle-nav">
-        <h3>{name}</h3>
+        <h3>{name} <span onClick={()=>{plus(heart+1)}}>❤️</span> {heart} </h3>
         <h4>{job}</h4>
         <h5>📞{call}</h5>
       </div>
