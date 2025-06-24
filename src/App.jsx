bb9d99d1d01ed800425301577d3966cd55bf6304
이성준 <seongjun060103@gmail.com>
2025-06-25 01:07:19 +0900

    feat-day1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function BusinessCard(a){
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h4>명함 </h4>
+        <p className="list">이름: {a.name} </p>
+        <p className="list">나이: {a.age} </p>
+        <p className="list">직업: {a.job} </p>
+        <p className="list">전화번호: {a.num} </p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
-export default App
+function App() {
+  return (
+    <div>
+      <BusinessCard
+        name="이성준"
+        age="20살"
+        job="대학생"
+        num="010-xxxx-xxxx"
+      />
+    </div>
+  );
+}
+
+export default App;
