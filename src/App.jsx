b2f80e17976195d98a653ae348383bb7b1a5bd56
jjjuun-debug <sungjun9709@naver.com>
2025-06-27 00:22:08 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,69 @@
 import logo from './logo.svg';
-import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
-function BusinessCard(a){
-  return (
-      <div>
-        <h4>명함 </h4>
-        <p className="list">이름: {a.name} </p>
-        <p className="list">나이: {a.age} </p>
-        <p className="list">직업: {a.job} </p>
-        <p className="list">전화번호: {a.num} </p>
-      </div>
+function Introducing(a){
+  return(
+    <div className = "box">
+     <h3>{a.name}</h3>
+      <p>{a.content}</p>
+    </div>
   )
 }
 
-function App() {
+function App(){
+  let [입력, 입력변경] = useState(['','','',''])
+  let [출력, 출력변경] = useState(['','','',''])
+
+  const input = (p,q) =>{
+    const copy = [...입력];
+    copy[p] = q
+    입력변경(copy)
+  }
+
   return (
     <div>
-      <BusinessCard
-        name="이성준"
-        age="20살"
-        job="대학생"
-        num="010-xxxx-xxxx"
+      <input
+        type="text"
+        placeholder="이름을 입력하세요"
+        onChange={(e) => input(0, e.target.value)}
+      />
+            <input
+        type="text"
+        placeholder="나이를 입력하세요"
+        onChange={(e) => input(1, e.target.value)}
+      />
+            <input
+        type="text"
+        placeholder="직업을 입력하세요"
+        onChange={(e) => input(2, e.target.value)}
+      />
+            <input
+        type="text"
+        placeholder="취미를 입력하세요"
+        onChange={(e) => input(3, e.target.value)}
+      />
+      
+      <div style={{ textAlign: 'center' }}>
+        <button onClick={()=>{ 출력변경([...입력])
+         }}>제출</button>
+      </div>
+
+      <Introducing
+        name='이름'
+        content={출력[0]}      
+      />
+      <Introducing
+        name='나이'
+        content={출력[1]}      
+      />
+      <Introducing
+        name='직업'
+        content={출력[2]}      
+      />
+      <Introducing
+        name='취미'
+        content={출력[3]}      
       />
     </div>
   );
