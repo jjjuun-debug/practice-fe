6c4f9cb441c6d33d7b33dc47e875e665dfe7d8d1
jjjuun-debug <seongjun060103@gmail.com>
2025-06-27 00:11:26 +0900

    feat-day3

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,69 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-function App() {
+function Introducing(a){
+  return(
+    <div className = "box">
+     <h3>{a.name}</h3>
+      <p>{a.content}</p>
+    </div>
+  )
+}
 
-  let [ 좋아요, 변경] = useState(0);
+function App(){
+  let [입력, 입력변경] = useState(['','','',''])
+  let [출력, 출력변경] = useState(['','','',''])
+
+  const input = (p,q) =>{
+    const copy = [...입력];
+    copy[p] = q
+    입력변경(copy)
+  }
 
   return (
     <div>
-      <div>
-        <h4><span onClick={()=>{ 변경(좋아요+1) }}>❤️ 좋아요</span> {좋아요}</h4>
+      <input
+        type="text"
+        placeholder="이름을 입력하세요"
+        onChange={(e) => input(0, e.target.value)}
+      />
+            <input
+        type="text"
+        placeholder="나이를 입력하세요"
+        onChange={(e) => input(1, e.target.value)}
+      />
+            <input
+        type="text"
+        placeholder="직업을 입력하세요"
+        onChange={(e) => input(2, e.target.value)}
+      />
+            <input
+        type="text"
+        placeholder="취미를 입력하세요"
+        onChange={(e) => input(3, e.target.value)}
+      />
+      
+      <div style={{ textAlign: 'center' }}>
+        <button onClick={()=>{ 출력변경([...입력])
+         }}>제출</button>
       </div>
+
+      <Introducing
+        name='이름'
+        content={출력[0]}      
+      />
+      <Introducing
+        name='나이'
+        content={출력[1]}      
+      />
+      <Introducing
+        name='직업'
+        content={출력[2]}      
+      />
+      <Introducing
+        name='취미'
+        content={출력[3]}      
+      />
     </div>
   );
 }
