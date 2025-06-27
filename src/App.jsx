2b77041cec93e0e1572f3c7c37f5a83848994bbc
jjjuun-debug <sungjun9709@naver.com>
2025-06-28 00:48:16 +0900

    feat-day4

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-function Introducing(a){
-  return(
-    <div className = "box">
-     <h3>{a.name}</h3>
-      <p>{a.content}</p>
-    </div>
-  )
-}
+export default function Input() {
+  const [인풋, 인풋변경] = useState('');
+  const [할일, 할일변경] = useState([]);
 
-function App(){
-  let [입력, 입력변경] = useState(['','','',''])
-  let [출력, 출력변경] = useState(['','','',''])
+  const send = () => {
+    const copy = { id: Date.now(), text: 인풋 };
+    할일변경([...할일, copy]);
+    인풋변경('');
+  };
 
-  const input = (p,q) =>{
-    const copy = [...입력];
-    copy[p] = q
-    입력변경(copy)
-  }
+      const 삭제 = (id) => {
+      할일변경(할일.filter((item) => item.id !== id));
+    };
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="이름을 입력하세요"
-        onChange={(e) => input(0, e.target.value)}
-      />
-            <input
-        type="text"
-        placeholder="나이를 입력하세요"
-        onChange={(e) => input(1, e.target.value)}
-      />
-            <input
-        type="text"
-        placeholder="직업을 입력하세요"
-        onChange={(e) => input(2, e.target.value)}
-      />
-            <input
-        type="text"
-        placeholder="취미를 입력하세요"
-        onChange={(e) => input(3, e.target.value)}
-      />
-      
-      <div style={{ textAlign: 'center' }}>
-        <button onClick={()=>{ 출력변경([...입력])
-         }}>제출</button>
-      </div>
+      <h2>TODO LIST</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="할 일 입력"
+          value={인풋}
+          onChange={(e) => 인풋변경(e.target.value)}
+          onKeyDown={(e) => e.key === 'Enter' && send()}
+        />
 
-      <Introducing
-        name='이름'
-        content={출력[0]}      
-      />
-      <Introducing
-        name='나이'
-        content={출력[1]}      
-      />
-      <Introducing
-        name='직업'
-        content={출력[2]}      
-      />
-      <Introducing
-        name='취미'
-        content={출력[3]}      
-      />
+        <button onClick={send}>추가</button>
+
+      </div>
+        <div>
+          {할일.map((a) => (
+          <div key={a.id}>
+            <span>‧ {a.text} </span>
+            <button onClick={()=>삭제(a.id)}>삭제</button>
+          </div>
+          ))}
+        </div>
     </div>
   );
 }
-
-export default App;
