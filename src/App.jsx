9b3e175be430909f2405496e2b59ff35446defc7
jjjuun-debug <sungjun9709@naver.com>
2025-06-30 21:33:06 +0900

    feat-day5

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
 import React, { useState } from 'react';
 
-function App() {
-  const [탭, 탭변경] = useState(1);
+export default function App() {
+  const images = [
+    {
+      id: 1,
+      src: 'https://previews.123rf.com/images/martialred/martialred1507/martialred150700888/42620709-landscape-photo-image-flat-icon-for-apps-and-websites.jpg',
+    },
+    {
+      id: 2,
+      src: 'https://previews.123rf.com/images/martialred/martialred1507/martialred150700661/42613290-landscape-photo-image-flat-icon-for-apps-and-websites.jpg',
+    },
+    {
+      id: 3,
+      src: 'https://previews.123rf.com/images/tkacchuk/tkacchuk1412/tkacchuk141200886/34580948-photo-archive-icon-on-white-background-vector-illustration.jpg',
+    },
+  ];
+
+  const [확대, 확대변경] = useState();
 
   return (
     <div>
-      <h2>탭</h2>
-      <button onClick={() => 탭변경(1)}>1</button>
-      <button onClick={() => 탭변경(2)}>2</button>
-      <button onClick={() => 탭변경(3)}>3</button>
+      <div>
+        {images.map((i) => (
+          <img
+            key={i.id}
+            src={i.src}
+            style={{
+              width: '100px',
+              height: '100px',
+              cursor: 'pointer',
+              border: '1px solid #ccc'
+            }}
+            onClick={() => 확대변경(i)}
+          />
+        ))}
+      </div>
 
-      {탭 === 1 && (
-        <div style={{ backgroundColor: 'red', height: '200px', marginTop: '10px' }}>{탭}</div>
-      )}
-
-      {탭 === 2 && (
-        <div style={{ backgroundColor: 'blue', height: '200px', marginTop: '10px' }}>{탭}</div>
-      )}
-
-      {탭 === 3 && (
-        <div style={{ backgroundColor: 'green', height: '200px', marginTop: '10px' }}>{탭}</div>
+      {확대 && (
+        <div>
+          <img
+            src={확대.src}
+            style={{ width: '400px', height: 'auto', border: '2px solid black' }}
+          />
+        </div>
       )}
     </div>
   );
 }
-
-export default App;
